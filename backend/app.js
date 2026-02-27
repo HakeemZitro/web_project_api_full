56f4a558dcf2c6d502380e9a12ad59cfe57944fd
Hakeem Ortiz <hzitrofelix@gmail.com>
2026-02-27 12:11:09 -0600

    Feature / Centralized error handling added

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ const { login, createUser } = require("./controllers/users.js");
 const { auth } = require("./middlewares/auth.js");
 require("dotenv").config();
 const { NODE_ENV, PORT } = process.env;
+const NotFoundError = require("./errors/not-found-err.js");
 
 
 const app = express();
@@ -22,7 +23,8 @@ app.use("/", auth, usersRouter);
 app.use("/", auth, cardsRouter);
 
 app.use((req, res, next) => {
-  res.status(404).send({ message: "Recurso solicitado no encontrado" })
+  const err = new NotFoundError("Recurso solicitado no encontrado");
+  next(err);
 });
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
